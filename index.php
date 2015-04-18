64773fc915c80c34b8f89106f1512dd8af57f94c
Sebastiaan Dirks <513811@lln.corlaercollege.nl>
2015-04-18 07:33:19 +0000

    Third commit

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <!-- Stylesheets -->
+        <link rel="stylesheet" href="css/materialize.min.css" type="text/css" />
+        <link rel="stylesheet" href="css/animate.css" type="text/css" />
         
+        <!-- Scripts -->
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+        <script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
     </head>
     
     <body>
